acdc2b32176d9521f6b40e42c92c6e844d6c5ded
Johan Gabriel <johan.universidade@gmail.com>
2026-05-26 21:37:22 -0300

    Aula 2605

--- tarefas/src/main/java/com/johan/tarefas/TarefasApplication.java
+++ tarefas/src/main/java/com/johan/tarefas/TarefasApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TarefasApplication {
+public class   TarefasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TarefasApplication.class, args);
--- tarefas/src/main/java/com/johan/tarefas/filter/SecurityFilter.java
+++ tarefas/src/main/java/com/johan/tarefas/filter/SecurityFilter.java
@@ -1,4 +1,19 @@
 package com.johan.tarefas.filter;
 
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityFilter {
+
+    public SecurityFilter securityFilterChain(HttpSecurity http) throws Exception{
+        return http .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests()
+    }
+
 }
