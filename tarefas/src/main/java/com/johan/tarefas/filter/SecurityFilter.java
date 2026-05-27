dd0b60a1cf947b00e65574bf774612cdb33e8bc2
Johan Gabriel <johan.universidade@gmail.com>
2026-05-26 22:13:39 -0300

    Atualizacoes

--- tarefas/src/main/java/com/johan/tarefas/filter/SecurityFilter.java
+++ tarefas/src/main/java/com/johan/tarefas/filter/SecurityFilter.java
@@ -1,19 +1,30 @@
 package com.johan.tarefas.filter;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityFilter {
 
-    public SecurityFilter securityFilterChain(HttpSecurity http) throws Exception{
-        return http .csrf(csrf -> csrf.disable())
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/pedidos").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/pedidos/**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/pedidos/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/pedidos/**").permitAll()
+                )
+                .build();
     }
 
 }
